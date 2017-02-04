c132a0561164d8b6af5cfa9294cb0d49fa10f9c4
MrMusician23 <inyourface1223@gmail.com>
2017-02-05 00:30:26 +0300

    Fixed layout for audio fragment

---
+++ app/src/main/java/com/sunflower/catchtherainbow/Views/AudioProgressView.java
@@ -0,0 +1,22 @@
+package com.sunflower.catchtherainbow.Views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by SuperComputer on 2/5/2017.
+ */
+
+public class AudioProgressView extends View
+{
+    public AudioProgressView(Context context)
+    {
+        super(context);
+    }
+
+    public AudioProgressView(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+    }
+}
