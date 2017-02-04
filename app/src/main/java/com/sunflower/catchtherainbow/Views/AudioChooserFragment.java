93804e53ab8b613d5ecfc5e14fdeec9b85294def
Solovyov23 <sascha.soloweow2012@yandex.ua>
2017-02-05 00:22:00 +0300

    Added Folder for Stan

--- app/src/main/java/com/sunflower/catchtherainbow/Views/AudioChooserFragment.java
+++ app/src/main/java/com/sunflower/catchtherainbow/Views/AudioChooserFragment.java
@@ -1,4 +1,4 @@
-package com.sunflower.catchtherainbow;
+package com.sunflower.catchtherainbow.Views;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.sunflower.catchtherainbow.R;
 
 /**
  * A simple {@link Fragment} subclass.
